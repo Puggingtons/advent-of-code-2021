9475d7df3f49e68edf2114a94db4f169a5cfcc9b
Paul Bader <paul.bader@rock5.de>
2021-12-29 20:43:14 +0100

    added progress to readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Advent of Code 2021
 Solutions to the coding challenge at https://adventofcode.com/2021 written in TypeScript.
+
+# Progress
+- [x] Day 1
+  - [x] Part 1
+  - [x] Part 2
+
+- [x] Day 2
+  - [x] Part 1
+  - [x] Part 2
+
+- [x] Day 3
+  - [x] Part 1
+  - [ ] Part 2
--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import Day01 from './01_sonar-sweep/Day01';
 import Day02 from './02_dive/Day02';
+import Day03 from './03_binary-diagnostic/Day03';
 import Day from './shared/Day';
 
-let day: Day = new Day02();
+let day: Day = new Day03();
 
 let resultPart1 = day.runPart1();
 console.log("The result of " + day.title + " - Part 1 is:");
